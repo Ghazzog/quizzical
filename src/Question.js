af2ff273671fd2b6efb2a83482b596cd63f0c30f
Hippo Alpha <renegadehippos@pm.me>
2022-08-16 01:46:26 +0300

    notworking

--- src/Question.js
+++ src/Question.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 export default function Question(props){
-    
+    const styles = {
+        backgroundColor: props.isHeld ? 'salmon' : ''
+    }
     return (
         <div className="question-wrap">
              <div>
-                    <h4>{props.question}</h4>
+                    <h4 key={props.qkey}>{props.question}</h4>
                     <ul>
-                        <button >{props.answer1}</button>
-                        <button >{props.answer2}</button>
-                        <button >{props.answer3}</button>
-                        <button >{props.answer4}</button>
+                        <button onClick={props.handleCli} style={styles} key={props.a1key}>{props.answer1}</button>
+                        <button onClick={props.handleClic} key={props.a2key}>{props.answer2}</button>
+                        <button onClick={props.handleClicc} key={props.a3key}>{props.answer3}</button>
+                        <button onClick={props.handleClicce} key={props.a4key}>{props.answer4}</button>
                     </ul>
                 </div>
         </div>
