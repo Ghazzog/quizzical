731d9fda0888d7d317f598665e33159a25801388
Hippo Alpha <renegadehippos@pm.me>
2022-08-17 17:23:24 +0300

    buttonUpdate

--- src/StartPage.js
+++ src/StartPage.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 function StartPage(props) {
+
+    
+
     return(
     <div className="quiz-wrapper">
         <h1>Quizzical</h1>
         <p>Let's test your knowledge</p>
-        <button className="mainButton" onClick={props.onChange}>Start the Game</button>
+        <button className="mainButton" onClick={props.showGame}>Start the Game</button>
     </div>
   
     )
